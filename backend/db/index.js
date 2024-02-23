4e5005e42184a4766f31307b18bb1e726bbab5c7
vishalsharma <vishalssharma811@gmail.com>
2024-02-23 15:12:05 +0530

    done

--- backend/db/index.js
+++ backend/db/index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const mongoose =require('mongoose');
+const { Schema } = require('zod');
 const url =process.env.MONGO_URL;
 mongoose.connect(url);
 
@@ -23,8 +24,23 @@ const userSchema =new mongoose.Schema({
     timestamps:true
 })
 
+// Account schema
+const accountSchema =new mongoose.Schema({
+     userId:{
+        type:mongoose.Schema.Types.ObjectId,  //Refrence to user model
+        ref:'User',
+        required:true
+     },
+     balance:{
+        type:Number,
+        required:true
+     }
+})
+
 const User =mongoose.model("User",userSchema)
+const Account =mongoose.model("Account",accountSchema)
 
 module.exports={
-    User
+    User,
+    Account
 }
